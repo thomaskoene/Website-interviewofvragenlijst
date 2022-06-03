bc5f13d7e8e9a0d3b2c90614e92c4be75149cc25
thomaskoene <54844862+thomaskoene@users.noreply.github.com>
2022-06-03 17:27:26 +0200

    cleaning some code

--- app.py
+++ app.py
@@ -4,34 +4,8 @@ from flask_sitemap import Sitemap
 # Configure application
 app = Flask(__name__)
 ext = Sitemap(app=app)
-# app.config["SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS"] = True
-# app.config["SITEMAP_URL_SCHEME"] = "https"
 
-# csp = {
-#     'default-src': [
-#         '\'self\'',
-#         '\'unsafe-inline\'',
-#         'stackpath.bootstrapcdn.com',
-#         'maxcdn.bootstrapcdn.com',
-#         'opinionstage.com/',
-#         'code.jquery.com',
-#         'cdn.jsdelivr.net',
-#         'w3.org'
-#     ],
-#     'frame-src': [
-#         "https://www.opinionstage.com/api/v1/widgets/1002786/iframe"
-#     ],
-#     'img-src': [
-#         '\'self\'',
-#         'https:',
-#         'data:'
-#     ]
-# }
-
-# Talisman(app, content_security_policy=csp)
 app.config["SECRET_KEY"] = b"\xa1\x13Ll+\x88(\xa4\xcf\xaa\xd7\x97'*j"
-
-
 
 @app.route("/")
 @app.route("/homescreen")
